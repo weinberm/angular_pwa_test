d69c001366789e1960c66a1f5068868857f219df
weinberger <smw1297@yahoo.de>
2023-04-27 22:53:29 +0200

    TEst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ILocation } from './ILocations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular_pwa_1';
+
+export class AppComponent implements OnInit {
+
+  offline: boolean = false;
+
+  public locations : ILocation[] = []
+
+  ngOnInit() {
+    this.offline = !navigator.onLine;
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  updateOnlineStatus() {
+    this.offline = !navigator.onLine;
+  }
+
+  getGeolocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // alert(
+          //   `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`
+          // );
+          var x : ILocation = {latitude :position.coords.latitude, longitude: position.coords.longitude}
+          this.locations.push(x)
+          console.log(this.locations)
+        },
+        (error) => {
+          alert(`Error getting location: ${error.message}`);
+        }
+      );
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }
 }
